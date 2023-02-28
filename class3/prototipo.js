938fd2982b6b307e08149bc1e6230556eec2f917
andredevsec <andre.all3@hotmail.com>
2023-02-27 21:42:44 -0300

    class3

---
+++ class3/prototipo.js
@@ -0,0 +1,13 @@
+function Foo () {
+
+}
+
+const foo = new Foo();
+
+const nome = "André" // String
+// const nome1 = String ("Dézinho Lima") // Stirng
+// const nome2 = new String ("Andrezinho do gueto") // Object
+
+const obj = new Object();
+
+console.log(nome.__proto__)
