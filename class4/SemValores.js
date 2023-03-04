d36c8e0e2797959cd2445d3005873c74aeff5957
andredevsec <andre.all3@hotmail.com>
2023-03-03 22:47:22 -0300

    Estudos sobre instaceof operações e variaveis

---
+++ class4/SemValores.js
@@ -0,0 +1,20 @@
+let entradaUm
+let entradaDois = undefined
+let entradaTres = null
+
+console.log(typeof entradaUm,typeof entradaDois, typeof entradaTres)
+console.log(entradaDois)
+
+console.log (entradaTres == entradaDois) // valore igual retorna true
+console.log(entradaTres === entradaDois) // valor e tipo ihgual === | retorna false
+
+console.log(entradaDois == false)
+
+console.log (entradaTres ? "Ok" : "false")
+
+if(entradaDois) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
